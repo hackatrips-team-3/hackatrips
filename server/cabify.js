dec80abeb79637f1e87561601e007fae98a9ed6e
gabriel <furstenheim@gmail.com>
2017-01-21 18:13:52 +0100

    Make a reservation

--- server/cabify.js
+++ server/cabify.js
@@ -1,51 +1,11 @@
-module.exports = {checkRoute}
+module.exports = {checkRoute, makeReservation}
 
 const config = require ('./config')
 const request = require('request')
 const url = require('url')
 // stops = {lat, lng}
 function checkRoute (stops, startAt) {
-  stops = [{lat: 40.4169473, lng:  -3.7057172}]
-  startAt = "2022-12-30 22:59"
   const apiStops = stops.map(s => {return {loc: [s.lat, s.lng]}})
-  const apiStops2 = [
-      {
-        "loc": [
-          40.4169473,
-          -3.7057172
-        ],
-        "name": "Puerta del Sol",
-        "addr": "Plaza de la Puerta del Sol",
-        "num": "s/n",
-        "city": "Madrid",
-        "country": "Spain",
-        "hit_at": "2022-12-30 22:59"
-      },
-    {
-      "loc": [
-        40.4169473,
-        -3.8057172
-      ],
-      "name": "Puerta del Sol",
-      "addr": "Plaza de la Puerta del Sol",
-      "num": "s/n",
-      "city": "Madrid",
-      "country": "Spain",
-      "hit_at": "2022-12-30 22:59"
-    },
-    {
-      "loc": [
-        40.4169473,
-        -3.6057172
-      ],
-      "name": "Puerta del Sol",
-      "addr": "Plaza de la Puerta del Sol",
-      "num": "s/n",
-      "city": "Madrid",
-      "country": "Spain",
-      "hit_at": "2022-12-30 22:59"
-    }
-    ]
 
   const options = {
     url: config.cabifyURL + '/estimate',
@@ -55,7 +15,7 @@ function checkRoute (stops, startAt) {
       'Accept-language': 'en'
     },
     body: {
-      stops: apiStops2,
+      stops: apiStops,
       start_at: startAt
     }
   }
@@ -64,6 +24,42 @@ function checkRoute (stops, startAt) {
   return new Promise(function (resolve, reject) {
     request.post(options, function (err, res) {
       if (err) return reject(err)
+      if (res.statusCode !== 200) {
+        return reject(new Error(res.body.message))
+      }
+      resolve(res.body)
+      console.log('cabify', res.body, res.statusCode)
+    })
+  })
+}
+
+function makeReservation (stops, startAt, rider, vehicle_type_id) {
+  // slice 0, 2 because the api is not yet ready for multiple journey
+  const apiStops = stops.map(s => {return {loc: [s.lat, s.lng]}}).slice(0, 2)
+
+  const body = {
+    stops: apiStops,
+    startAt,
+    rider,
+    vehicle_type_id
+  }
+  const options = {
+    url: config.cabifyURL + '/journey',
+    json: true,
+    headers: {
+      Authorization: `Bearer ${config.cabifyToken}`,
+      'Accept-language': 'en'
+    },
+    body: body
+  }
+
+  return new Promise(function (resolve, reject) {
+    request.post(options, function (err, res) {
+      if (err) return reject(err)
+      if (res.statusCode !== 200) {
+        console.log(res.body.errors)
+        return reject(new Error(res.body.message))
+      }
       resolve(res.body)
       console.log('cabify', res.body, res.statusCode)
     })
