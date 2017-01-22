6b8dc031ae3b1992887c06dfd229b03b8134bd7e
Guli <ignaciomorenopubul@gmail.com>
2017-01-22 10:06:28 +0100

    LUIS integrations

--- server/app.js
+++ server/app.js
@@ -11,7 +11,67 @@ const connector = new builder.ChatConnector({
 
 const bot = new builder.UniversalBot(connector)
 
-bot.dialog('/', dispatcher)
+//LUIS natural language processing
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/f854c21e-a18f-474c-862d-29ceee773b36?subscription-key=e7721191ec97407db8be3179d6cc8dc9&verbose=true';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+//Intent handlers
+dialog.matches('Greetings', builder.DialogAction.send('Hello to you :smile: more_detail. Where do you wanna go?'));
+
+dialog.matches('BookTaxi', [
+    function (session, args, next) {
+    	console.log(args)
+        // Resolve and store any entities passed from LUIS.
+        var location = builder.EntityRecognizer.findEntity(args.entities, 'Location');
+        var time = builder.EntityRecognizer.findEntity(args.entities, 'timeUntilReady');
+        console.log(location, time)
+        var booking = {
+        	time: time ? time.entity : null,
+        	location: location ? location.entity : null
+        }
+        session.dialogData.booking = booking
+
+        // Prompt for title
+        if (!booking.location) {
+            builder.Prompts.text(session, 'What\'s your destination?')
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+    	console.log('second block', session)
+    	var booking = session.dialogData.booking
+		if (results.response) {
+			session.beginDialog('/fromPrompt')
+			console.log('RESPONSE', results)
+			booking.location = results.response
+        } 
+
+        if (booking.location && !booking.time){
+        	builder.Prompts.text(session, 'When do you want your cab?')
+        } else {
+        	next()
+        }
+
+
+    },
+    function (session, results) {
+        var booking = session.dialogData.booking;
+        if (results.response) {
+            booking.time = results.response
+            console.log('TIME', booking.time)
+        }
+
+        if (booking.location && booking.time) {
+           session.send('Ok! Cab to ' + booking.location + ' ' + booking.time) 
+        }
+    }
+]);
+
+dialog.matches('cancelBooking', builder.DialogAction.send('You got it. Booking cancelled'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
 
 const server = restify.createServer()
 server.listen(8080)
--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  appID: 'My app',
-  appPassword: 'pass',
+  appID: 'eb199fab-19ec-4dad-b41f-2084fbddceb5',
+  appPassword: 'AXJCLhoTAq5GoCmEi7VDfjJ',
   cabifyToken: 'r3Z0pCB9dusjQsNFFBnaTmGkgzEBJBmc9r7w0wFLNKc',
   cabifyURL: 'https://test.cabify.com/api/v2'
 }
+
